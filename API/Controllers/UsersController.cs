7945ab495efed88a749d848077fe6bad28e67097
lorentag90 <lorentag90@outlook.com>
2021-04-13 21:53:28 -0700

    End of Section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -17,7 +17,7 @@ namespace API.Controllers
             _context = context;
         }
 
-        [Authorize]
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
             return await _context.Users.ToListAsync();            
